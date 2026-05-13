a090b70d58a254e6fa53a147da767f0a31dd681a
rajishan2005 <rajishan2005@gmail.com>
2026-05-13 14:58:11 +0530

    Add /start command and refactor join handler

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ChatJoinRequestHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ChatJoinRequestHandler,
+    CommandHandler,
+    ContextTypes
+)
+
 import asyncio
 
 BOT_TOKEN = "8952613119:AAH18318ilnpQgxJ_1qQtUGg0d6qHokx_t0"
 CHANNEL_ID = -1003806202683
 
-async def joined(update: Update, context):
+# /start command
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hi! Bot is running perfectly.")
+
+# Join request handler
+async def joined(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.chat_join_request.from_user
 
     print(f"{user.username} joined")
@@ -16,7 +27,7 @@ async def joined(update: Update, context):
         user_id=user.id
     )
 
-    # Allow 60 seconds access
+    # Allow access for 60 seconds
     await asyncio.sleep(60)
 
     # Permanently ban user
@@ -29,7 +40,10 @@ async def joined(update: Update, context):
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
+# Register handlers
+app.add_handler(CommandHandler("start", start))
 app.add_handler(ChatJoinRequestHandler(joined))
 
 print("Bot running...")
+
 app.run_polling()
